d0d1dc969f453c4d7b45294aeeb16a06eaaaa111
chrisroselli <chrisroselli@gmail.com>
2020-06-22 15:41:42 -0400

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let babel = require("gulp-babel");
+let concat = require("gulp-concat");
+let rename = require('gulp-rename');
+let uglify = require("gulp-uglify");
+
 
 function minifycss() {
   return gulp.src('css/*.css')
@@ -13,15 +17,20 @@ function minifycss() {
           },
       },
   }))
-
+  .pipe(concat('bundle.css'))
   .pipe(gulp.dest('dist'));
 }
 
-function babeljs() {
+function minifyjs() {
   return gulp.src("js/*.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+  .pipe(babel({
+    presets: ['@babel/env']
+  }))
+  .pipe(gulp.dest("dist"))
+  .pipe(rename('uglify.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest("dist"));
 }
 
-const build = gulp.series(minifycss,babeljs);
+const build = gulp.series(minifycss,minifyjs);
 exports.default = build;
